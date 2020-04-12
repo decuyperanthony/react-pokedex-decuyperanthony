39a78ec49194d6e61c05cdd0f78ae0537f21fcf0
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-04-12 22:23:09 +0200

    add pokemon name on click in store and in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 // == styles
 import './styles.scss';
 import { Segment } from 'semantic-ui-react';
 
 
-const Header = () => (
-  <Segment><header>Header</header></Segment>
-);
+const Header = () => {
+  const pokemon = useSelector((state) => state.pokemonName);
+  console.log('header', pokemon);
+  return (
+    <Segment><header>Header {pokemon}</header></Segment>
+  );
+};
+
 
 export default Header;
